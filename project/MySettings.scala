594c6466a81a1ee1d96d5453d5012ffffdfd24f6
Scala Steward <me@scala-steward.org>
2021-05-28 11:39:05 +0200

    Update scala-library to 2.12.14

--- project/MySettings.scala
+++ project/MySettings.scala
@@ -8,7 +8,7 @@ import xerial.sbt.Sonatype.SonatypeKeys
 object MySettings {
 
   lazy val scala213 = "2.13.6"
-  lazy val scala212 = "2.12.13"
+  lazy val scala212 = "2.12.14"
   lazy val scala211 = "2.11.12"
 
   lazy val commonSettings = Seq(
